9720589c45afbe2e472feb23d4c43023b43cf89a
GuilhermeSams <guilhermeps3670@gmail.com>
2023-10-01 17:07:47 -0300

    pequenas alterações e função de empate

--- velha.cpp
+++ velha.cpp
@@ -23,3 +23,13 @@ int verificarVencedorO(int jogo[3][3]) {
             return 2;
     }
 }
+
+int verificarEmpate(int jogo[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (jogo[i][j] == 0)
+                return 0;
+        }
+    }
+    return 0;  // Se não há posição vazia, o jogo está empatado
+}
