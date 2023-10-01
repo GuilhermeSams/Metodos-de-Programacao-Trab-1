affaaa2210b149930c6e93d2600ab2983e83e354
GuilhermeSams <guilhermeps3670@gmail.com>
2023-10-01 16:00:37 -0300

    pronto pra começar os testes

--- velha.cpp
+++ velha.cpp
@@ -1,8 +1,8 @@
 #include "velha.hpp"
 
 /** 
- * @brief verifica situacao do jogo da velha
- * @author Guilherme Pereira Sampaio 
+ * @brief verifica situacao do jogo da velha  
+ * @author Guilherme Pereira Sampaio
  * @param  velha descreve o parametro
  * 
  *  Descrever o que a funcao faz
